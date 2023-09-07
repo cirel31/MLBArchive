c4e2adfd2623d3dcbb16111fc97470c083253458
정도현 <cirel31@gmail.com>
2023-09-07 10:27:56 +0900

    [S09P22A301-46] test: 로그인 oauth 테스트 중

--- frontend/app/oauth/naver/page.tsx
+++ frontend/app/oauth/naver/page.tsx
@@ -1,13 +1,27 @@
 'use client'
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const NaverLoginPage = () => {
-  const code = window.location.href.split("code=")[1]?.split("&")[0]
+  const API_KEY_NAVER = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID
+  const API_SECRETE_NAVER = process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET
+  const parsingData = window.location.href.split("code=")[1]?.split("&")[0]
+  const code = parsingData
   console.log(code)
   const [isLoading, setIsLoading] = useState(true);
   const baseURL = '';
   const subURL = '';
-
+  useEffect(() => {
+    if (code) {
+      axios.get(`https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${API_KEY_NAVER}&client_secret=${API_SECRETE_NAVER}&code=${code}`)
+        .then((response) => {
+          console.log(response.data)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }, [code])
   return (
     <>
       <div>
