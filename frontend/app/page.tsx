211aee42561b6b65efdefa4c81f11a3ade184d3c
peachez-z <rkfld0911@gmail.com>
2023-09-07 12:55:09 +0900

    fix page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,19 @@
 import Link from "next/link";
+import "../styles/MainPageStyle.scss";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href={'/login'}>
-        <div>
-          로그인 페이지
-        </div>
+      <div className="mainphoto">
+        <img
+          src="https://img.mlbstatic.com/mlb-images/image/upload/t_16x9/t_w2208/mlb/xqq7yvk8nshiq4jvkmqq.jpg"
+          alt=""
+        />
+      </div>
+
+      <Link href={"/login"}>
+        <div>로그인 페이지</div>
       </Link>
     </main>
-  )
+  );
 }
