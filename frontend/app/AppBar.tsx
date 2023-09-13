1914514d0ed41fea6583bc6264e7af23a853a6c8
peachez-z <rkfld0911@gmail.com>
2023-09-13 15:44:38 +0900

    fix 네비바

--- frontend/app/AppBar.tsx
+++ frontend/app/AppBar.tsx
@@ -1,12 +1,16 @@
 "use client";
 import { useRouter } from "next/navigation";
 import "../styles/AppBarStyle.scss";
+import intro from "../assets/intro/intro.png";
+import Image from "next/image";
+
 const AppBar = () => {
   const router = useRouter();
 
   return (
     <>
-      <div className="flex w-full justify-around appbar">
+      <div className="appbar">
+        <Image className="mark" src={intro} alt="" />
         <button onClick={() => router.push("/")}>Home</button>
         <button onClick={() => router.push("/teams")}>teams</button>
         <button onClick={() => router.push("/players")}>players</button>
--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,17 +1,18 @@
-'use client'
-import {useEffect, useRef, useState} from "react";
-import "../../styles/LoginPageStyle.scss"
-import Image from 'next/image'
-import kakao_btn from "../../assets/btn/kakao_btn_login.png"
-import google_btn_dark from "../../assets/btn/google_btn_dark.png"
-import naver_btn_light from "../../assets/btn/naver_btn_light.png"
+"use client";
+import { useEffect, useRef, useState } from "react";
+import "../../styles/LoginPageStyle.scss";
+import Image from "next/image";
+import kakao_btn from "../../assets/btn/kakao_btn_login.png";
+import google_btn_dark from "../../assets/btn/google_btn_dark.png";
+import naver_btn_light from "../../assets/btn/naver_btn_light.png";
+import intro from "../../assets/intro/intro_pull.png";
 
 const LoginPage = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [STATE, SETSTATE] = useState('')
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [STATE, SETSTATE] = useState("");
   const generateState = () => {
-    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
+    const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
 
     for (let i = 0; i < 20; i++) {
       const randomIndex = Math.floor(Math.random() * characters.length);
@@ -19,39 +20,50 @@ const LoginPage = () => {
     }
 
     return result;
-  }
+  };
 
   useEffect(() => {
-    SETSTATE(generateState())
-  }, [])
+    SETSTATE(generateState());
+  }, []);
 
-  const BASE_URL = process.env.NEXT_PUBLIC_CLIENT_BASE_URL
+  const BASE_URL = process.env.NEXT_PUBLIC_CLIENT_BASE_URL;
 
-  const API_KEY_KAKAO = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID
-  const REDIRECT_URI_KAKAO = process.env.NEXT_PUBLIC_OAUTH_KAKAO_URL
+  const API_KEY_KAKAO = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
+  const REDIRECT_URI_KAKAO = process.env.NEXT_PUBLIC_OAUTH_KAKAO_URL;
   const OAUTH_KAKAO = `https://kauth.kakao.com/oauth/authorize?client_id=${API_KEY_KAKAO}&redirect_uri=${BASE_URL}${REDIRECT_URI_KAKAO}&response_type=code&state=${STATE}`;
 
-  const API_KEY_GOOGLE = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID
-  const REDIRECT_URI_GOOGLE = process.env.NEXT_PUBLIC_OAUTH_GOOGLE_URL
-  const OAUTH_GOOGLE = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${API_KEY_GOOGLE}&redirect_uri=${BASE_URL}${REDIRECT_URI_GOOGLE}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email`
+  const API_KEY_GOOGLE = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+  const REDIRECT_URI_GOOGLE = process.env.NEXT_PUBLIC_OAUTH_GOOGLE_URL;
+  const OAUTH_GOOGLE = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${API_KEY_GOOGLE}&redirect_uri=${BASE_URL}${REDIRECT_URI_GOOGLE}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email`;
 
-  const API_KEY_NAVER = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID
-  const REDIRECT_URI_NAVER = process.env.NEXT_PUBLIC_OAUTH_NAVER_URL
-  const OAUTH_NAVER = `https://nid.naver.com/oauth2.0/authorize?client_id=${API_KEY_NAVER}&redirect_uri=${BASE_URL}${REDIRECT_URI_NAVER}&response_type=code&state=${STATE}`
+  const API_KEY_NAVER = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
+  const REDIRECT_URI_NAVER = process.env.NEXT_PUBLIC_OAUTH_NAVER_URL;
+  const OAUTH_NAVER = `https://nid.naver.com/oauth2.0/authorize?client_id=${API_KEY_NAVER}&redirect_uri=${BASE_URL}${REDIRECT_URI_NAVER}&response_type=code&state=${STATE}`;
 
   return (
-    <div className="loginContents">
-      <a href={OAUTH_NAVER} className="loginItem">
-        <Image src={naver_btn_light} alt="네이버 로그인" className="oauthLogo" />
-      </a>
-      <a href={OAUTH_KAKAO} className="loginItem">
-        <Image src={kakao_btn} alt="카카오 로그인" className="oauthLogo" />
-      </a>
-      <a href={OAUTH_GOOGLE} className="loginItem">
-        <Image src={google_btn_dark} alt="구글 로그인" className="oauthLogo" />
-      </a>
+    <div>
+      <Image src={intro} className="logo" alt="" />
+      <div className="loginContents">
+        <a href={OAUTH_NAVER} className="loginItem">
+          <Image
+            src={naver_btn_light}
+            alt="네이버 로그인"
+            className="oauthLogo"
+          />
+        </a>
+        <a href={OAUTH_KAKAO} className="loginItem">
+          <Image src={kakao_btn} alt="카카오 로그인" className="oauthLogo" />
+        </a>
+        <a href={OAUTH_GOOGLE} className="loginItem">
+          <Image
+            src={google_btn_dark}
+            alt="구글 로그인"
+            className="oauthLogo"
+          />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default LoginPage;
