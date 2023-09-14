556fc63f3ab5df2ac2a3d71264eb822ec19a9c80
codelee <leesy399@naver.com>
2023-09-14 14:41:26 +0900

    chore Lee : 선수 검색 로직 구현

--- backend/ssafyBigswing/src/main/java/com/example/ssafy301/player/controller/PlayerController.java
+++ backend/ssafyBigswing/src/main/java/com/example/ssafy301/player/controller/PlayerController.java
@@ -3,12 +3,12 @@ package com.example.ssafy301.player.controller;
 import com.example.ssafy301.common.api.ResponseEntity;
 import com.example.ssafy301.common.api.status.SuccessCode;
 import com.example.ssafy301.player.dto.PlayerDetailDto;
+import com.example.ssafy301.player.dto.PlayerDto;
 import com.example.ssafy301.player.service.PlayerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +21,20 @@ public class PlayerController {
     @GetMapping("/detail/{playerId}")
     public ResponseEntity getDetailPlayer(@PathVariable("playerId") Long playerId) {
         PlayerDetailDto result = playerService.getDetailPlayer(playerId);
+        return ResponseEntity.success(SuccessCode.GENERAL_SUCCESS, result);
+    }
+    
+    // 첫 영문자로 선수 검색
+    @GetMapping("/search/{firstletter}")
+    public ResponseEntity searchPlayerByFirstletter(Pageable pageable, @PathVariable("firstletter") char firstletter) {
+        Page<PlayerDto> result = playerService.searchPlayerByFirstletter(pageable, firstletter);
+        return ResponseEntity.success(SuccessCode.GENERAL_SUCCESS, result);
+    }
+    
+    // 2글자 이상의 문자열로 선수 검색
+    @GetMapping("/search/{content}")
+    public ResponseEntity searchPlayerByName(Pageable pageable, @PathVariable("content") String content) {
+        Page<PlayerDto> result = playerService.searchPlayerByName(pageable, content);
         return ResponseEntity.success(SuccessCode.GENERAL_SUCCESS, result);
     }
 }
