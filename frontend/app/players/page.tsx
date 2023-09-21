f7608afc7f74d2fba16d0c8321c52e067a3e0d8c
peachez-z <rkfld0911@gmail.com>
2023-09-21 16:25:17 +0900

    player page

--- frontend/app/players/page.tsx
+++ frontend/app/players/page.tsx
@@ -1,12 +1,16 @@
-"use client"
-import sample from "../../assets/player/sample_profile_img.jpg"
-import Image, {StaticImageData} from "next/image";
-import {useState} from "react";
-import "../../styles/SearchPlayerPageStyle.scss"
-import {useDispatch, useSelector} from "react-redux";
-import {fetchPlayerLetterData, fetchPlayerWordData} from "@/app/redux/features/searchPlayerSlice";
-import {useRouter} from "next/navigation";
+"use client";
+import sample from "../../assets/player/sample_profile_img.jpg";
+import Image, { StaticImageData } from "next/image";
+import { useState } from "react";
+import "../../styles/SearchPlayerPageStyle.scss";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchPlayerLetterData,
+  fetchPlayerWordData,
+} from "@/app/redux/features/searchPlayerSlice";
+import { useRouter } from "next/navigation";
 import playerFace from "@/app/players/[...id]/current.png";
+import { Helmet } from "react-helmet";
 
 type PlayerType = {
   id: number;
@@ -17,46 +21,73 @@ type PlayerType = {
   teamCode: string;
 };
 const AllPlayers = () => {
-  const dispatch = useDispatch()
-  const router = useRouter()
-  const searchLetter = useSelector((state: any) => state.searchPlayer.firstLetterList)
-  const totalPage = useSelector((state: any) => state.searchPlayer.totalPage)
-  const [nowPage, setNowPage] = useState(0)
-  const [searchData, setSearchData] = useState('')
-  const articlePerPage = 30
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const searchLetter = useSelector(
+    (state: any) => state.searchPlayer.firstLetterList
+  );
+  const totalPage = useSelector((state: any) => state.searchPlayer.totalPage);
+  const [nowPage, setNowPage] = useState(0);
+  const [searchData, setSearchData] = useState("");
+  const articlePerPage = 30;
   const [isActive, setIsActive] = useState([
-    false, false, false, false, false, false,
-    false, false, false, false, false, false,
-    false, false, false, false, false, false,
-    false, false, false, false, false, false,
-    false, false
-  ])
-  const alphabets =  Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i));
-  const backgroundCheck = (idx:number) => {
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+  const alphabets = Array.from({ length: 26 }, (_, i) =>
+    String.fromCharCode(65 + i)
+  );
+  const backgroundCheck = (idx: number) => {
     const updatedIsActive = isActive.map((_, index) => index === idx);
     setIsActive(updatedIsActive);
-    const selectedAlphabet = alphabets[idx]
-    setSearchData(selectedAlphabet)
+    const selectedAlphabet = alphabets[idx];
+    setSearchData(selectedAlphabet);
     const action = {
-      searchData: selectedAlphabet, nowPage: nowPage, articlePerPage: articlePerPage
-    }
-    dispatch(fetchPlayerLetterData(action))
-  }
-  const searchQuery = (x:number) => {
+      searchData: selectedAlphabet,
+      nowPage: nowPage,
+      articlePerPage: articlePerPage,
+    };
+    dispatch(fetchPlayerLetterData(action));
+  };
+  const searchQuery = (x: number) => {
     if (x === 0 || nowPage + x < 0) {
-      setNowPage(0)
-    }
-    else if (nowPage + x >= totalPage || x === totalPage) {
-      setNowPage(totalPage-1)
-    }
-    else {
-      setNowPage(nowPage+x)
+      setNowPage(0);
+    } else if (nowPage + x >= totalPage || x === totalPage) {
+      setNowPage(totalPage - 1);
+    } else {
+      setNowPage(nowPage + x);
     }
     const action = {
-      searchData: searchData, nowPage:nowPage, articlePerPage:30
-    }
-    dispatch(fetchPlayerLetterData(action))
-  }
+      searchData: searchData,
+      nowPage: nowPage,
+      articlePerPage: 30,
+    };
+    dispatch(fetchPlayerLetterData(action));
+  };
 
   const handleDetailPage = (id: string) => {
     router.push(`players/${id}`);
@@ -84,10 +115,14 @@ const AllPlayers = () => {
     setBackgroundStyle(null);
   };
 
-  const cards = ["charizard", "mewtwo", "dragonite"]; // Add more card names if needed
-
   return (
     <>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+        />
+      </Helmet>
       <div className="searchAlphabet">
         {alphabets.map((alphabet, index) => (
           <div
@@ -99,117 +134,70 @@ const AllPlayers = () => {
           </div>
         ))}
       </div>
-      <div>
-        {searchLetter ? searchLetter.map((player: any) => (
-          <div key={player.id} className='playerContent' onClick={() => handleDetailPage(player.id)}>
-            <img src={player.image} alt={player.name} className='profile'/>
-            <div>
-              <p>{player.name}</p>
-              <p>{player.team} {player.number}</p>
-            </div>
-          </div>
-
-        )) :
-          <div>알파벳을 선택해 주세요.</div>
-        }
-        <div>
-          <button onClick={() => searchQuery(0)}>시작 페이지</button>
-          <br/>
-          <br/>
-          <button onClick={() => searchQuery(-1)}>이전 페이지</button>
-          <br/>
-          <br/>
-          <button onClick={() => searchQuery(+1)}>다음 페이지</button>
-          <br/>
-          <br/>
-          <button onClick={() => searchQuery(totalPage)}>끝 페이지</button>
-        </div>
-      </div>
-      <div className="cardzone">
-        <main id="app">
-          <div className="card charizard">
-            <div className="playerCard">
-              <div className="container">
-                <div className="rectangle2">이름</div>
-                <Image
-                  src={playerFace}
-                  alt=""
-                  className="playerFace"
-                  style={{ borderRadius: "10px" }}
-                />
-
-                <div className="rectangle">
-                  <div className="circleBox">
-                    <div>
-                      <div className="circle">1</div>
-                      <div className="circle2">2</div>
-                    </div>
-                    <div>
-                      <div className="circle3">3</div>
-                      <div className="circle4">4</div>
+      <div className="totalCard">
+        {searchLetter ? (
+          searchLetter.map((player: any) => (
+            <div
+              key={player.id}
+              className="playerContent"
+              onClick={() => handleDetailPage(player.id)}
+            >
+              <div>
+                {/* <p>{player.name}</p> */}
+                <p>
+                  {player.team} {player.number}
+                </p>
+                <main id="app">
+                  <div className="card charizard">
+                    <div className="playerCard">
+                      <div className="container">
+                        <div className="rectangle2">{player.name}</div>
+                        <img
+                          src={player.image}
+                          alt={player.name}
+                          className="playerFace"
+                        />
+                        <div className="rectangle">
+                          <div className="circleBox">
+                            <div>
+                              <div className="circle">{player.number}</div>
+                              <div className="circle2">2</div>
+                            </div>
+                            <div>
+                              <div className="circle3">3</div>
+                              <div className="circle4">4</div>
+                            </div>
+                            <p>{player.team}</p>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
+                </main>
               </div>
             </div>
-          </div>
-        </main>
-        <main id="app">
-          <div className="card charizard">
-            <div className="playerCard">
-              <div className="container">
-                <Image src={playerFace} alt="" className="playerFace" />
-                <div className="circle">1</div>
-                <div className="circle2">2</div>
-                <div className="circle3">3</div>
-                <div className="rectangle">어쩌구</div>
-                <div className="rectangle2">선수이름</div>
-              </div>
-            </div>
-          </div>
-        </main>
-        <main id="app">
-          <div className="card charizard">
-            <div className="playerCard">
-              <div className="container">
-                <Image src={playerFace} alt="" className="playerFace" />
-                <div className="circle">1</div>
-                <div className="circle2">2</div>
-                <div className="circle3">3</div>
-                <div className="rectangle">어쩌구</div>
-                <div className="rectangle2">선수이름</div>
-              </div>
-            </div>
-          </div>
-        </main>
-        <main id="app">
-          <div className="card charizard">
-            <div className="playerCard">
-              <div className="container">
-                <Image src={playerFace} alt="" className="playerFace" />
-                <div className="circle">1</div>
-                <div className="circle2">2</div>
-                <div className="circle3">3</div>
-                <div className="rectangle">어쩌구</div>
-                <div className="rectangle2">선수이름</div>
-              </div>
-            </div>
-          </div>
-        </main>
-        <main id="app">
-          <div className="card charizard">
-            <div className="playerCard">
-              <div className="container">
-                <Image src={playerFace} alt="" className="playerFace" />
-                <div className="circle">1</div>
-                <div className="circle2">2</div>
-                <div className="circle3">3</div>
-                <div className="rectangle">어쩌구</div>
-                <div className="rectangle2">선수이름</div>
-              </div>
-            </div>
-          </div>
-        </main>
+          ))
+        ) : (
+          <div>알파벳을 선택해 주세요.</div>
+        )}
+      </div>
+      <div>
+        <button onClick={() => searchQuery(0)}>
+          <span className="material-symbols-outlined">
+            keyboard_double_arrow_left
+          </span>
+        </button>
+        <button onClick={() => searchQuery(-1)}>
+          <span className="material-symbols-outlined">keyboard_backspace</span>
+        </button>
+        <button onClick={() => searchQuery(+1)}>
+          <span className="material-symbols-outlined">arrow_right_alt</span>
+        </button>
+        <button onClick={() => searchQuery(totalPage)}>
+          <span className="material-symbols-outlined">
+            keyboard_double_arrow_right
+          </span>
+        </button>
       </div>
     </>
   );
