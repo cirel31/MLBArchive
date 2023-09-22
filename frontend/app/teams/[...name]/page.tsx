3dfb77e8ca7a4aba7b970fb825bda14e38866ae9
정도현 <cirel31@gmail.com>
2023-09-22 12:24:29 +0900

    feature: 팀데이터 기본 api로직 설정
    1. 상세 페이지 접근 시 id로 팀 로고 및 트위터 링크 출력

--- frontend/app/teams/[...name]/page.tsx
+++ frontend/app/teams/[...name]/page.tsx
@@ -1,24 +1,40 @@
 "use client";
 import { useEffect, useState } from "react";
 import "../../../styles/TeamPage.css";
+import {useSelector} from "react-redux";
+import Image from "next/image";
+import {selectLogo, selectTwitter} from "@/app/components/team/teamData";
 
 const DetailTeamPage = () => {
-  const [teamName, setTeamName] = useState("");
+  const [isLoading, setIsLoading] = useState(true)
+  const teamDetailData = useSelector((state:any) => state.team.teamData)
+  const searchId = teamDetailData?.id ?? 120
+  const logoPath = selectLogo(searchId)
+  const twitterPath = selectTwitter(searchId)
   useEffect(() => {
-    const parsing = window.location.href.split("teams/")[1];
-    setTeamName(parsing);
-  }, []);
-
+    if (teamDetailData) {
+      setIsLoading(false)
+    }
+  })
+  if (isLoading) {
+    console.log("로딩 중...")
+    // return (
+    //   <>
+    //     <div>로딩 중...</div>
+    //   </>
+    // )
+  }
   return (
     <>
       <div className="container">
         <div className="item1">
           <p>팀로고</p>
+          <Image src={logoPath} alt="이미지 없음" />
         </div>
         <div className="item2">
-          <h1>{teamName}</h1>
           <p>아무거나 팀 이미지 하나씩 넣기</p>
           <p> 팀 트위터 넣고 싶음</p>
+          <a href={twitterPath}>트위터 넣어 줌</a>
         </div>
         <div className="item3">
           <p>팀관련기사</p>
