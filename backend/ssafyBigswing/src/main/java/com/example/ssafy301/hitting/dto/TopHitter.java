3aced1a850963c3ad5261be4828894a62e6464fb
codelee <leesy399@naver.com>
2023-09-25 14:43:26 +0900

    chore lee: 메인페이지 Hitting과 Pitching Top5 명단을 보낼 때 playerId와 korName도 함께 보냄 & 시뮬레이션 추천로직에서 teamName을 teamId로 바꿈

--- backend/ssafyBigswing/src/main/java/com/example/ssafy301/hitting/dto/TopHitter.java
+++ backend/ssafyBigswing/src/main/java/com/example/ssafy301/hitting/dto/TopHitter.java
@@ -6,14 +6,18 @@ import lombok.Data;
 @Data
 public class TopHitter {
 
-    // 선수사진, 선수이름, 타율을 반환
+    // 선수 id, 선수사진, 선수이름(영어,한국), 타율을 반환
+    private Long playerId;
     private String image;
     private String name;
+    private String korName;
     private float battingAvg;
 
     public TopHitter(Hitting hitting) {
+        this.playerId = hitting.getPlayer().getId();
         this.image = hitting.getPlayer().getImage();
         this.name = hitting.getPlayer().getName();
+        this.korName = hitting.getPlayer().getKorName();
         this.battingAvg = hitting.getBattingAvg();
     }
 }
--- backend/ssafyBigswing/src/main/java/com/example/ssafy301/pitching/dto/TopPitcher.java
+++ backend/ssafyBigswing/src/main/java/com/example/ssafy301/pitching/dto/TopPitcher.java
@@ -6,14 +6,18 @@ import lombok.Data;
 @Data
 public class TopPitcher {
 
-    // 선수사진, 선수이름, 방어율을 반환
+    // 선수id, 선수사진, 선수이름(영어, 한국), 방어율을 반환
+    private Long playerId;
     private String image;
     private String name;
+    private String korName;
     private float era;
 
     public TopPitcher(Pitching pitching) {
+        this.playerId = pitching.getPlayer().getId();
         this.image = pitching.getPlayer().getImage();
         this.name = pitching.getPlayer().getName();
+        this.korName = pitching.getPlayer().getKorName();
         this.era = pitching.getEra();
     }
 }
