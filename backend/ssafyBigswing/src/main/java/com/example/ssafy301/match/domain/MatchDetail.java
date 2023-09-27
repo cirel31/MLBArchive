1dd321b4aa7b6fcec8874b1de217f11b2a92d4d8
kanghee96 <kanghee96@gmail.com>
2023-09-27 13:49:33 +0900

    feat: 로스터 구현

--- backend/ssafyBigswing/src/main/java/com/example/ssafy301/match/domain/MatchDetail.java
+++ backend/ssafyBigswing/src/main/java/com/example/ssafy301/match/domain/MatchDetail.java
@@ -3,7 +3,6 @@ package com.example.ssafy301.match.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,8 +19,8 @@ public class MatchDetail {
 
     @Column(name = "match_id")
     private Long matchId;
-    @Lob
+    @Column(columnDefinition = "LONGTEXT")
     private String linescore;
-    @Lob
+    @Column(columnDefinition = "LONGTEXT")
     private String boxscore;
 }
