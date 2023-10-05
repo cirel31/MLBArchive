92e52cc047dfe46bbeb0d10c7b4045fdd603cc36
정도현 <cirel31@gmail.com>
2023-10-05 10:44:35 +0900

    Update frontend/Dockerfile, frontend/app/login/page.tsx

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -37,8 +37,7 @@ const LoginPage = () => {
   const API_KEY_KAKAO = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
   const REDIRECT_URI_KAKAO = process.env.NEXT_PUBLIC_OAUTH_KAKAO_URL;
   const KAKAO_URL = `${BASE_URL}${REDIRECT_URI_KAKAO}`
-  console.log("카카오 경로 진행 중", REDIRECT_URI_KAKAO)
-  const OAUTH_KAKAO = `https://kauth.kakao.com/oauth/authorize?client_id=${API_KEY_KAKAO}&redirect_uri=${REDIRECT_URI_KAKAO}&response_type=code&state=${STATE}`;
+  const OAUTH_KAKAO = `https://kauth.kakao.com/oauth/authorize?client_id=${API_KEY_KAKAO}&redirect_uri=${KAKAO_URL}&response_type=code&state=${STATE}`;
 
 
   const API_KEY_GOOGLE = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
