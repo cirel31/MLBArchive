eec0fe070a9d80ffa9f7cf167352f2e76ae38a7a
정도현 <cirel31@gmail.com>
2023-10-05 13:41:48 +0900

    fix: 프론트 D-day 수정 중

--- frontend/app/match/[...id]/page.tsx
+++ frontend/app/match/[...id]/page.tsx
@@ -2,15 +2,29 @@
 import MatchData from "@/app/components/match/matchData";
 import MatchLineScore from "@/app/components/match/matchLineScore";
 import "../../../styles/MatchLineScore.css";
+import {useEffect} from "react";
+import {requestDetailMatchData} from "@/app/redux/features/matchSlice";
+import {useDispatch, useSelector} from "react-redux";
 
 const MatchPage = () => {
+  const dispatch = useDispatch()
+  const matchData = useSelector((state:any) => state.match.matchDetailData)
+  useEffect(() => {
+    const parsing = window.location.href.split("match/")[1];
+    console.log(parsing)
+    dispatch(requestDetailMatchData(parsing));
+  }, [])
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <div className="match_box ">
-        <MatchLineScore />
-        <MatchData />
-      </div>
-    </div>
+    <>
+      {matchData &&
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div className="match_box ">
+            <MatchLineScore />
+            <MatchData />
+          </div>
+        </div>
+      }
+    </>
   );
 };
 
